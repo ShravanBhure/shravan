84a7014719f9ece414f2fb834ea06b3816c0e7aa
Shravan Bhure <149852262+ShravanBhure@users.noreply.github.com>
2025-04-29 19:52:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const io = new Server(server, {
 });
 
 const users = {};
-const userToSocket = {}; 
+const userToSocket = {};
 
 io.on('connection', socket => {
     socket.on('new-user-joined', (loggedInUser) => {
@@ -23,8 +23,8 @@ io.on('connection', socket => {
           oldSocket.disconnect(true); // force disconnect
         }
       }
-  
-  
+
+
       users[socket.id] = loggedInUser;
       userToSocket[loggedInUser] = socket.id;
 
@@ -33,31 +33,31 @@ io.on('connection', socket => {
         io.emit('update-user-list', Object.values(users));
       });
 
-      socket.on("send", (message) => {
-        const loggedInUser = users[socket.id];
-        if (loggedInUser) {
-          socket.broadcast.emit("receive", {
-            message,
-            loggedInUser
-          });
-        }
-      });
-    
-
-    socket.on('disconnect', () => {
+    socket.on("send", (message) => {
       const loggedInUser = users[socket.id];
-      delete users[socket.id];
-      if (userToSocket[loggedInUser] === socket.id) {
-        delete userToSocket[loggedInUser];
+      if (loggedInUser) {
+        socket.broadcast.emit("receive", {
+          message,
+          loggedInUser
+        });
       }
-      console.log(`${loggedInUser} left the chat`);
-
-      socket.broadcast.emit('left', loggedInUser);
-      io.emit('update-user-list', Object.values(users));
     });
+
+
+  socket.on('disconnect', () => {
+    const loggedInUser = users[socket.id];
+    delete users[socket.id];
+    if (userToSocket[loggedInUser] === socket.id) {
+      delete userToSocket[loggedInUser];
+    }
+    console.log(`${loggedInUser} left the chat`);
+
+    socket.broadcast.emit('left', loggedInUser);
+    io.emit('update-user-list', Object.values(users));
   });
+});
 
 // Start server on port 8000
-server.listen(8000, () => {
-  console.log("Server running on http://localhost:8000");
+server.listen(8000, '0.0.0.0', () => { // <-- Changed 'localhost' to '0.0.0.0'
+  console.log("Server running on http://localhost:8000"); // This log message is still fine
 });
